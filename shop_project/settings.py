6a56b846fb723ac1bb1261bd10b5d19c2bf41311
Deanf13245 <x00171562@mytudublin.ie>
2021-11-15 22:21:30 +0000

    change password

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -125,6 +125,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
